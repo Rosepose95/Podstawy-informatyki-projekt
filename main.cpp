0ba7fc2e118e0e7de0fab5999f22f1da0105cff3
Rosepose95 <s.mekal@wp.pl>
2025-12-27 12:57:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include "Game.h"
 #include "Enemy.h"
 #include "Tower.h"
+#include "Map.h"
+
 
 int main() {
     sf::RenderWindow window(
-        sf::VideoMode({ 800, 600 }),
+        sf::VideoMode({ 1240,840 }),
         "Tower Defense SFML 3"
     );
 
     Game game;
+    Map map;    //
 
-    game.addEnemy(Enemy(100, 0.f, 100.f));
-    game.addEnemy(Enemy(120, 0.f, 200.f));
+    game.addEnemy(Enemy(200, 0.f, 80.f));
+    game.addEnemy(Enemy(300, 0.f, 720.f));  //pozmienialem pozycje zeby nie byly pomiedzy bloczkami 
+    game.addEnemy(Enemy(200, 0.f, 80.f));
+    game.addEnemy(Enemy(200, 1040.f, 800.f));
 
 
-    game.addTower(Tower(20, 400.f, 150.f));
-    game.addTower(Tower(30, 600.f, 300.f));
+    game.addTower(Tower(20, 560.f, 440.f));
+    //game.addTower(Tower(30, 600.f, 300.f));
 
     sf::Clock clock;
 
@@ -35,7 +40,13 @@ int main() {
                     sf::Vector2f worldPos =
                         window.mapPixelToCoords(mouse->position);
 
-                    game.placeTower(worldPos);
+                    //zaokroglam aby wieze nie mozna bylo postawic pomiedzy kratkami
+                    int tileSize = map.tileSize;
+                    float gridX = (int)worldPos.x / tileSize * tileSize;
+                    float gridY = (int)worldPos.y / tileSize * tileSize;
+
+                    game.placeTower({ gridX, gridY });
+
                 }
             }
         }
@@ -45,9 +56,13 @@ int main() {
         game.update(dt);
 
         window.clear(sf::Color::White);
+
+        map.draw(window);//
+
         game.draw(window);
         window.display();
     }
 
     return 0;
 }
+
