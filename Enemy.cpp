406b2c01eb112ead8805bd27fc411b22bd10c7c2
EmiliaNowakowsk <enowak.b@gmail.com>
2025-12-27 13:52:40 +0100

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,11 +1,12 @@
 #include "Enemy.h"
 
-Enemy::Enemy(int hp, float x, float y)
-    : health(hp) {
+Enemy::Enemy(int h, float startX, float startY)
+    : health(h), maxHealth(h) {    //dodatek życia
     shape.setRadius(20.f);
     shape.setFillColor(sf::Color::Red);
-    shape.setPosition(sf::Vector2f(x, y));
+    shape.setPosition({ startX, startY }); //zmiana zapisu
 }
+
 
 void Enemy::move(float dx, float dy) {
     shape.move(sf::Vector2f(dx, dy));
@@ -18,11 +19,35 @@ void Enemy::takeDamage(int dmg) {
 bool Enemy::isDead() const {
     return health <= 0;
 }
+#include <algorithm> // Do rysowania paska zdrowia
 
 void Enemy::draw(sf::RenderWindow& window) const {
     window.draw(shape);
+
+    float ratio = static_cast<float>(health) / maxHealth;
+    ratio = std::clamp(ratio, 0.f, 1.f);
+
+    sf::Vector2f pos = shape.getPosition();
+
+    sf::RectangleShape backBar({ 40.f, 5.f });
+    backBar.setFillColor(sf::Color::Red);
+    backBar.setPosition({ pos.x, pos.y - 10.f });
+
+    sf::RectangleShape hpBar({ 40.f * ratio, 5.f });
+    hpBar.setFillColor(sf::Color::Green);
+    hpBar.setPosition({ pos.x, pos.y - 10.f });
+
+    window.draw(backBar);
+    window.draw(hpBar);
 }
 
 sf::Vector2f Enemy::getPosition() const {
     return shape.getPosition();
+}
+
+float Enemy::getRadius() const { //metody do paska
+    return shape.getRadius();
+}
+int Enemy::getMaxHealth() const {
+    return maxHealth;
 }
