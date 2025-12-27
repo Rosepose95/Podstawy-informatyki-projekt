5d2ced4f048417a1dd3ef919bd9bc185ed3ede0e
EmiliaNowakowsk <enowak.b@gmail.com>
2025-12-27 13:54:59 +0100

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -7,16 +7,19 @@ class Enemy {
 private:
     int health;
     sf::CircleShape shape;
-
+    int maxHealth;  //życie
 public:
     Enemy(int hp, float x, float y);
+    float getRadius() const;  //by znać średnicę
 
     void move(float dx, float dy);
     void takeDamage(int dmg);
     bool isDead() const;
+    int getMaxHealth() const;
 
     void draw(sf::RenderWindow& window) const;
     sf::Vector2f getPosition() const;
 };
 
 #endif
+
