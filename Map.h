9befc39fb4cfc25b5ae05640b39baec4bacdb3d6
Rosepose95 <s.mekal@wp.pl>
2025-12-27 12:57:57 +0100

    Update Map.h

--- Map.h
+++ Map.h
@@ -2,17 +2,22 @@
 #define MAP_H
 
 #include <vector>
+#include <SFML/Graphics.hpp>
 
 class Map {
 public:
     Map();
+    void draw(sf::RenderWindow& window);
+
     char getTile(int x, int y) const;
     int getWidth() const;
     int getHeight() const;
 
+    int tileSize = 40; // Rozmiar kafelka (800/20 = 40, 640/16 = 40)
+
 private:
+
     std::vector<std::vector<char>> grid;
 };
 
 #endif
-
