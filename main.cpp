f459a13200b602e00c6e8e8a0ca5d6b844fae343
EmiliaNowakowsk <enowak.b@gmail.com>
2025-12-28 19:43:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,58 +14,70 @@ int main() {
     Game game;
     Map map;    //
     game.setMap(&map);
+    game.startNextWave(); //nowa fala
 
     int ts = map.tileSize;
-    game.addEnemy(Enemy(200, 0.f, 80.f));
-    game.addEnemy(Enemy(300, 0.f, 720.f));  //pozmienialem pozycje zeby nie byly pomiedzy bloczkami 
-    game.addEnemy(Enemy(200, 0.f, 80.f));
-    game.addEnemy(Enemy(200, 1040.f, 800.f));
+   game.addEnemy(
+    Enemy(
+        200,
+        0 * ts + ts / 2.f,
+        2 * ts + ts / 2.f
+    )
+);
 
 
-    game.addTower(Tower(20, 560.f, 440.f));
-    //game.addTower(Tower(30, 600.f, 300.f));
+// TOWER (na trawie) 
+game.addTower(
+    Tower(
+        20,
+        14 * ts + ts / 2.f,
+        11 * ts + ts / 2.f
+    )
+);
 
     sf::Clock clock;
+     while (window.isOpen()) {
 
-    while (window.isOpen()) {
+     while (auto ev = window.pollEvent()) {
 
-        while (auto ev = window.pollEvent()) {
+         if (ev->is<sf::Event::Closed>()) {
+             window.close();
+         }
 
-            if (ev->is<sf::Event::Closed>()) {
-                window.close();
-            }
+         if (const auto* mouse =
+             ev->getIf<sf::Event::MouseButtonPressed>()) {
 
-            if (const auto* mouse =
-                ev->getIf<sf::Event::MouseButtonPressed>()) {
+             if (mouse->button == sf::Mouse::Button::Left) {
+                 sf::Vector2f worldPos =
+                     window.mapPixelToCoords(mouse->position);
+                 //zaokroglam aby wieze nie mozna bylo postawic pomiedzy kratkami
+                 int tileSize = map.tileSize;
+                 float tileX = static_cast<int>(worldPos.x) / tileSize;
+                 float tileY = static_cast<int>(worldPos.y) / tileSize;
+               //by było wyśrodkowane
+                 float centerX = tileX * tileSize + tileSize / 2.f;
+                 float centerY = tileY * tileSize + tileSize / 2.f;
 
-                if (mouse->button == sf::Mouse::Button::Left) {
-                    sf::Vector2f worldPos =
-                        window.mapPixelToCoords(mouse->position);
+                 game.placeTower({ centerX, centerY });
+             }
+         }
+     }
 
-                    //zaokroglam aby wieze nie mozna bylo postawic pomiedzy kratkami
-                    int tileSize = map.tileSize;
-                    float gridX = (int)worldPos.x / tileSize * tileSize;
-                    float gridY = (int)worldPos.y / tileSize * tileSize;
-
-                    game.placeTower({ gridX, gridY });
-
-                }
-            }
-        }
-
+   
 
         float dt = clock.restart().asSeconds();
         game.update(dt);
 
         window.clear(sf::Color::White);
-
         map.draw(window);//
 
         game.draw(window);
+        game.drawUI(window); //dodatek ui
         window.display();
     }
 
     return 0;
 }
+
 
 
