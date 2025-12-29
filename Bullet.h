7213dec7590a383c6e9245d2c05b2918df202663
EmiliaNowakowsk <enowak.b@gmail.com>
2025-12-29 17:46:14 +0100

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -6,25 +6,27 @@
 class Bullet {
 private:
     sf::CircleShape shape;
-    sf::Vector2f direction;  //zmiana zmiennych 
+    sf::Vector2f direction; //zmiana zmiennych 
     float speed;
     float lifetime;
     int damage;
     bool dead;
 
 public:
-   Bullet(sf::Vector2f start, sf::Vector2f target);
+	Bullet(sf::Vector2f start, sf::Vector2f target, int dmg);  //poprawka do konstruktora
 
     void update(float dt);
     void draw(sf::RenderWindow& window) const;
-    bool isDead() const;
 
-    void kill();    //usuwanie użytych pocisków i ich pozycja
+    bool isDead() const;
+    void kill(); //usuwanie użytych pocisków i ich pozycja
+
     sf::Vector2f getPosition() const;
     int getDamage() const;
 };
 
 #endif
+
 
 
 
