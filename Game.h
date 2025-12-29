866f3c9f2b68e3669ee5f14564f97b07a35d1734
EmiliaNowakowsk <enowak.b@gmail.com>
2025-12-29 18:01:13 +0100

    Update Game.h

--- Game.h
+++ Game.h
@@ -7,58 +7,62 @@
 #include "Tower.h"
 #include "Bullet.h"
 #include "Map.h" //dodanie nagłówka
-
-struct WaveConfig { //kostrukcja do fal
+struct WaveConfig {  //kostrukcja do fal
     int count;
     int enemyHP;
     float speed;
     float spawnDelay;
 };
 
+
 class Game {
 private:
     std::vector<Enemy> enemies;
     std::vector<Tower> towers;
     std::vector<Bullet> bullets;
-    Map* map = nullptr; //zmiany by działała mapa, życia, fale i spawner
+    Map* map = nullptr;  //zmiany by działała mapa, życia, fale i spawner
     int playerLives = 20;
     int baseHP;
     bool gameOver;
     // WAVES
     int currentWave = 0;
     int enemiesToSpawn = 0;
-    int enemiesAlive = 0;
     WaveConfig currentWaveConfig;
-
     float spawnTimer = 0.f;
     float spawnDelay = 0.6f; // co ile sekund enemy
 
-     // UI
+    // UI
     sf::Font font;
-    sf::Text livesText;
     sf::Text waveText;
     sf::Text enemiesText;
+    sf::Text livesText;
+
+    bool isBossWave = false;
+    bool waveInProgress = false;
+
 
 public:
     Game();
-    void setMap(Map* m);  //ustawianie mapy i blokady stawiania wież i rozpoczęcie nowej fali
+    void setMap(Map* m); //ustawianie mapy i blokady stawiania wież i rozpoczęcie nowej fali
     void addEnemy(Enemy enemy);
     void startNextWave();
+    void updateUI();
 
     void addTower(Tower tower);
     void placeTower(sf::Vector2f position);
     bool canPlaceTower(sf::Vector2f pos) const;
-   
 
     void update(float dt);
     void draw(sf::RenderWindow& window) const;
-    void drawUI(sf::RenderWindow& window) const; //do ui
+    void drawUI(sf::RenderWindow& window) const;  //do ui
 
     int getBaseHP() const;
     bool isGameOver() const;
 };
 
 #endif
+
+
 
 
 
