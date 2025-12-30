04f8cc548ac0baca5f86edbbff6f0d1785fa48ed
Rosepose95 <s.mekal@wp.pl>
2025-12-30 02:12:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>#include <SFML/Graphics.hpp>
 #include "Game.h"
 #include "Enemy.h"
 #include "Tower.h"
 #include "Map.h"
+#include "menu.h"
 
+enum class GameStates {MENU, PLAYING}; //aby menu dzialalo
 
 int main() {
     sf::RenderWindow window(
@@ -13,41 +15,46 @@ int main() {
 
     Game game;
     Map map;
-
     game.setMap(&map);
-    game.startNextWave();
+    
+    sf::Font font;
+    font.openFromFile("assets/ArialMT.ttf");
+    Menu menu(1240.f, 840.f, font);
 
-    int ts = map.tileSize;
-
-    game.addEnemy(
-        Enemy(
-            200,
-            0 * ts + ts / 2.f,
-            2 * ts + ts / 2.f
-        )
-    );
-
-
-    // TOWER (na trawie) 
-    game.addTower(
-        Tower(
-            20,
-            14 * ts + ts / 2.f,
-            11 * ts + ts / 2.f
-        )
-    );
+    bool isMenuOpen = true;
+    
 
     sf::Clock clock;
 
     while (window.isOpen()) {
+        sf::Vector2i mousepos = sf::Mouse::getPosition(window);
 
         while (auto ev = window.pollEvent()) {
 
             if (ev->is<sf::Event::Closed>()) {
                 window.close();
             }
+            //menu
+            if (isMenuOpen) {
+                if (ev->is<sf::Event::MouseButtonPressed>()) {
+                    if (menu.isStartClicked(mousepos)) {
+                        isMenuOpen = false; //zamkniecie menu
 
-            if (const auto* mouse =
+                        game.startNextWave();
+                        int ts = map.tileSize;
+
+                        game.addEnemy(
+                            Enemy(200, 0 * ts + ts / 2.f, 2 * ts + ts / 2.f));
+
+                        //TOWER (na trawie) 
+                        game.addTower(
+                            Tower(20, 14 * ts + ts / 2.f, 11 * ts + ts / 2.f));
+
+                    }
+                }
+            }
+
+            else if (const auto* mouse =
                 ev->getIf<sf::Event::MouseButtonPressed>()) {
 
                 if (mouse->button == sf::Mouse::Button::Left) {
@@ -71,13 +78,20 @@ int main() {
         game.update(dt);
 
         window.clear(sf::Color::White);
-        map.draw(window);//
 
-        game.draw(window);
-        game.drawUI(window);  //dodatek ui
+        if (isMenuOpen) {
+            menu.handleHover(mousepos);
+            menu.draw(window);
+        }
+        else {
+            map.draw(window);//
+            game.draw(window);
+            game.drawUI(window);  //dodatek ui
+        }
 
         window.display();
     }
 
     return 0;
 }
+
