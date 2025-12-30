0c2532cc6c3f6185084c2cd1b09dd51c1fba913d
Rosepose95 <s.mekal@wp.pl>
2025-12-30 19:30:09 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,3 +1,4 @@
+
 #ifndef MENU_H
 #define MENU_H
 #include <SFML/Graphics.hpp>
@@ -7,14 +8,17 @@ private:
 
 	sf::Text title;
 	sf::Text startText;
+	sf::Text exitText;
 	sf::RectangleShape startButton;
-	sf::Sprite backgroundSpirite;
+	sf::RectangleShape exitButton;//
 	sf::Texture backgroundTexture;	//dla grafiki na tle
+	sf::Sprite backgroundSpirite;
 
 public:
 	Menu(float width, float height, sf::Font& arial);
 	void draw(sf::RenderWindow& window);
 	bool isStartClicked(sf::Vector2i mousepos);
+	bool isExitClicked(sf::Vector2i mousepos);
 	void handleHover(sf::Vector2i mousepos);	//podswietlanie jak sie najedzie
 
 };
