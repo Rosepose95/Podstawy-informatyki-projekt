3aca6e0a87b834ae39bf5fa9014c3e390e3d6863
Rosepose95 <s.mekal@wp.pl>
2026-01-02 19:52:43 +0100

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -6,6 +6,7 @@ Menu::Menu(float width, float height, sf::Font& arial)
 	: title(arial)
 	, startText(arial)
 	, exitText(arial)
+	, editText(arial)
 	, backgroundSpirite(backgroundTexture)
 { 
 	if (!backgroundTexture.loadFromFile("assets/menugraf.png")) {
@@ -41,6 +42,11 @@ Menu::Menu(float width, float height, sf::Font& arial)
 		exitButton.setOutlineThickness(3.f);
 		exitButton.setOutlineColor(sf::Color::Black);
 
+		//przycisk editButton
+		editButton.setSize({ 250.f, 70.f });
+		editButton.setOutlineThickness(3.f);
+		editButton.setOutlineColor(sf::Color::Black);
+
 		//wysrodkowanie guzika startButton
 		auto c = startButton.getLocalBounds();
 		startButton.setOrigin({ c.position.x + c.size.x / 2.f, c.position.y + c.size.y / 2.f });
@@ -49,8 +55,12 @@ Menu::Menu(float width, float height, sf::Font& arial)
 		//wysrodkowanie guzika exitButton
 		auto d = exitButton.getLocalBounds();
 		exitButton.setOrigin({ d.position.x + d.size.x / 2.f, d.position.y + d.size.y / 2.f });
-		exitButton.setPosition({ width / 2.f, height * 0.65f });
+		exitButton.setPosition({ width / 2.f, height * 0.85f });
 
+		//wysrodkowanie guzika editButton
+		auto g = editButton.getLocalBounds();
+		editButton.setOrigin({ g.position.x + g.size.x / 2.f, g.position.y + g.size.y / 2.f });
+		editButton.setPosition({ width / 2.f, height * 0.65f });
 
 	//tekst na przycisku startButton
 	startText.setString("START");
@@ -64,6 +74,12 @@ Menu::Menu(float width, float height, sf::Font& arial)
 	exitText.setFillColor(sf::Color::White);
 	exitText.setStyle(sf::Text::Bold);
 
+	//tekst na przycisku editButton
+	editText.setString("Make your map");
+	editText.setCharacterSize(30);
+	editText.setFillColor(sf::Color::White);
+	editText.setStyle(sf::Text::Bold);
+
 	//wysrodkowanie napisu na guziku startButton
 	auto e = startText.getLocalBounds();
 	startText.setOrigin({ e.position.x + e.size.x / 2.f, e.position.y + e.size.y / 2.f });
@@ -72,7 +88,12 @@ Menu::Menu(float width, float height, sf::Font& arial)
 	//wysrodkowanie napisu na guziku exitButton
 	auto f = exitText.getLocalBounds();
 	exitText.setOrigin({ f.position.x + f.size.x / 2.f, f.position.y + f.size.y / 2.f });
-	exitText.setPosition({ width / 2.f, height * 0.65f });
+	exitText.setPosition({ width / 2.f, height * 0.85f });
+
+	//wysrodkowanie napisu na guziku exitButton
+	auto h = editText.getLocalBounds();
+	editText.setOrigin({ h.position.x + h.size.x / 2.f, h.position.y + h.size.y / 2.f });
+	editText.setPosition({ width / 2.f, height * 0.65f });
 
 }
 void Menu::draw(sf::RenderWindow& window) {
@@ -82,6 +103,8 @@ void Menu::draw(sf::RenderWindow& window) {
 	window.draw(title);
 	window.draw(exitButton);
 	window.draw(exitText);
+	window.draw(editButton);
+	window.draw(editText);
 }
 void Menu::handleHover(sf::Vector2i mousepos){
 	sf::Vector2f mousePosF((int)mousepos.x, (int)mousepos.y);
@@ -100,6 +123,13 @@ void Menu::handleHover(sf::Vector2i mousepos){
 	else {
 		exitButton.setFillColor(sf::Color::Blue);
 	}
+
+	if (editButton.getGlobalBounds().contains(mousePosF)) {
+		editButton.setFillColor(sf::Color::Cyan);
+	}
+	else {
+		editButton.setFillColor(sf::Color::Blue);
+	}
 }
 bool Menu::isStartClicked(sf::Vector2i mousepos) {
 	sf::Vector2f mousePosF((int)mousepos.x, (int)mousepos.y);
@@ -110,4 +140,9 @@ bool Menu::isExitClicked(sf::Vector2i mousepos) {
 	sf::Vector2f mousePosF((int)mousepos.x, (int)mousepos.y);
 
 	return exitButton.getGlobalBounds().contains(mousePosF) && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
+}
+bool Menu::isEditClicked(sf::Vector2i mousepos) {
+	sf::Vector2f mousePosF((int)mousepos.x, (int)mousepos.y);
+
+	return editButton.getGlobalBounds().contains(mousePosF) && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
 }
