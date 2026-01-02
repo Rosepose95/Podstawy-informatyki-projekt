6baedac58cbaf81cc898a8f3e14ef234bfdca131
Rosepose95 <s.mekal@wp.pl>
2026-01-02 19:51:49 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -9,8 +9,11 @@ private:
 	sf::Text title;
 	sf::Text startText;
 	sf::Text exitText;
+	sf::Text editText;
 	sf::RectangleShape startButton;
 	sf::RectangleShape exitButton;//
+	sf::RectangleShape editButton;
+
 	sf::Texture backgroundTexture;	//dla grafiki na tle
 	sf::Sprite backgroundSpirite;
 
@@ -19,7 +22,10 @@ public:
 	void draw(sf::RenderWindow& window);
 	bool isStartClicked(sf::Vector2i mousepos);
 	bool isExitClicked(sf::Vector2i mousepos);
+	bool isEditClicked(sf::Vector2i mousepos);
+
 	void handleHover(sf::Vector2i mousepos);	//podswietlanie jak sie najedzie
 
 };
 #endif
+
