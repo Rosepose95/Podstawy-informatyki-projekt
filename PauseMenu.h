76efeacae2e314c3136640ffd7bef2e758bc7816
EmiliaNowakowsk <enowak.b@gmail.com>
2026-01-03 20:59:13 +0100

    Update PauseMenu.h

--- PauseMenu.h
+++ PauseMenu.h
@@ -2,6 +2,14 @@
 #define PAUSE_MENU_H
 
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <string>
+
+enum class PauseScreen {
+    MAIN,
+    SAVE_SLOTS,
+    LOAD_SLOTS
+};
 
 class PauseMenu {
 private:
@@ -11,8 +19,15 @@ private:
     sf::Text resumeText;
     sf::Text restartText;
     sf::Text menuText;
+    sf::Text saveText;
+    sf::Text loadText;   // ? TO JEST OK
+    std::vector<sf::Text> slotButtons;
+    sf::Text backText;
+
 
 public:
+    PauseScreen currentScreen = PauseScreen::MAIN;
+
     PauseMenu(float width, float height, sf::Font& font);
 
     void draw(sf::RenderWindow& window);
@@ -21,7 +36,12 @@ public:
     bool resumeClicked(sf::Vector2i mousePos);
     bool restartClicked(sf::Vector2i mousePos);
     bool menuClicked(sf::Vector2i mousePos);
+    bool saveClicked(sf::Vector2i mousePos);
+    bool loadClicked(sf::Vector2i mousePos);
+
+    int slotClicked(sf::Vector2i mousePos);
+    void setSlotText(int index, const std::string& text);
+    bool backClicked(sf::Vector2i mousePos);
+    
 };
-
 #endif
-
