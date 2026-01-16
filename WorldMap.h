5d7f4ac265986d38e6aaf17deb03d1f604271ecf
EmiliaNowakowsk <enowak.b@gmail.com>
2026-01-16 22:27:48 +0100

    Update WorldMap.h

--- WorldMap.h
+++ WorldMap.h
@@ -5,14 +5,28 @@
 struct WorldMapNode {
     std::string id;
     sf::FloatRect rect;
+    float unlockAnim = 0.f; 
 };
+struct WorldMapLink {
+    int from;
+    int to;
+};
+
+
 
 class WorldMap {
 public:
     void init();
+    void update(const AdventureMode& adv, float dt);
     void draw(sf::RenderWindow& win, const AdventureMode& adv) const;
     bool handleClick(sf::Vector2f pos, AdventureMode& adv, std::string& outNodeId);
     std::string getNodeAt(sf::Vector2f pos) const;
 private:
     std::vector<WorldMapNode> nodes;
+    sf::RectangleShape background;
+    sf::Font font;
+    std::vector<float> unlockAnims;
+    std::vector<WorldMapLink> links;
+   
+
 };
