ad04e290ea4dd148818a9008747c0f450d719e48
EmiliaNowakowsk <enowak.b@gmail.com>
2026-01-16 22:16:08 +0100

    Update AdventureMode.h

--- AdventureMode.h
+++ AdventureMode.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include <unordered_map>
-
+#include "Biomes.h"
 struct AdventureNode {
     std::string id;
     Biomes biome;
@@ -15,18 +15,18 @@ class AdventureMode {
 public:
     void completeNode(const std::string& id);
     void setNodeState(const std::string& id, bool unlocked, bool completed);
-    
+
     void init();
     bool isUnlocked(const std::string& id) const;
     bool isCompleted(const std::string& id) const;
     std::string getNextNode(const std::string& id) const;
     int getRewardGold(const std::string& nodeId) const;
     bool isBossNode(const std::string& id) const;
-    Biomes getBiomeForNode(const std::string& nodeId);
+    Biomes getBiomeForNode(const std::string& id) const;
 
 
 private:
     std::unordered_map<std::string, AdventureNode> nodes;
+	int baseGoldReward = 25;
 };
-
 
